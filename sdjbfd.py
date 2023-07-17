0c9208b0f7d7d3de52c222463a468649ca84005a
Biplab Sarkar <94435217+Biplabsarkar2018b@users.noreply.github.com>
2023-07-17 17:08:17 +0530

    frist

--- sdjbfd.py
+++ sdjbfd.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-d = {'col1':[1,2,3,4,67],'col2':[4,5,6,9,5],'col3':[7,8,12,1,11]}
+d = {'col1': [1, 2, 3, 4, 67], 'col2': [
+    4, 5, 6, 9, 5], 'col3': [7, 8, 12, 1, 11]}
 df = pd.DataFrame(data=d)
 # print(df)
 print(df.shape[1])
